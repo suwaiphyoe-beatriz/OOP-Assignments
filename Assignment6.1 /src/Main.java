ce28ff39cf271ae0206269593113457688aa753a
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-11-22 23:39:56 +0200

    Assignment 6.1 completed

---
+++ Assignment6.1 /src/Main.java
@@ -0,0 +1,11 @@
+package src;
+import src.view.DictionaryView;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryView.launch(DictionaryView.class);
+    }
+}
+
+
+
